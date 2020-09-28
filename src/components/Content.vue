6382614a2b62519f39d3af7c8ac7117e80433998
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2020-09-28 16:32:20 -0300

    initial project

---
+++ src/components/Content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ pokeapi }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data(){
+    return{
+      pokeapi: []
+    }
+  },
+  methods:{
+    feachApi(){
+      this.pokeapi = fetch('http://localhost:3030/')
+      .then( res => res.json())
+      .then( res => {
+        console.log(res)
+      })
+    }
+  },
+  created(){
+    this.feachApi()
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
