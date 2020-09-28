776017eb3f7f709daaa9cea049ab274b9c57f88b
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2020-09-28 19:20:14 -0300

    Update feach

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>{{ pokeapi }}</div>
+  <div>{{ pokeapi.results }}
+    <ul>
+      <li v-for="(item, index) in pokeapi" :key="index">
+       <p>{{index+1}} {{item.name}}</p>
+       <p>{{item.url}}</p>
+       <div>
+         
+       </div>
+      </li>
+    </ul>
+    <form>
+      <select :input="feachGeneration(generation)" v-model="generation">
+        <option v-for="(item, index) in pokeapi" :key="index" :value="index+1" :selected="index == 0">
+          {{item.name}}
+        </option>
+      </select>
+    </form>
+    <p>
+      {{ generation }}
+      {{ bodyGeneration }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -7,16 +28,30 @@ export default {
   name: 'Content',
   data(){
     return{
-      pokeapi: []
+      pokeapi: [],
+      item:'',
+      generation:'',
+      bodyGeneration: []
     }
   },
   methods:{
-    feachApi(){
-      this.pokeapi = fetch('http://localhost:3030/')
+    async feachApi(){
+      const data = await fetch("https://pokeapi.co/api/v2/generation")
       .then( res => res.json())
       .then( res => {
-        console.log(res)
+        console.log(res.results)
+      this.pokeapi = res.results
       })
+      return data
+    },
+    async feachGeneration(id){
+      const data = await fetch(`https://pokeapi.co/api/v2/generation/${id}`)
+      .then( res => res.json())
+      .then( res => {
+        this.bodyGeneration = res
+        console.log(this.bodyGeneration)
+      })
+      return data
     }
   },
   created(){
